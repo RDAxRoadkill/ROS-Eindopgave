df5258e0a3d5805471af66d814b9e1acce308549
Wouter Jansen <wouter97@planet.nl>
2019-11-25 11:50:01 +0100

    Update again

--- src/listener.py
+++ src/listener.py
@@ -3,6 +3,7 @@ import sys
 import rospy
 import subprocess
 import math
+import time
 from std_msgs.msg import String
 from geometry_msgs.msg import Point, Twist
 from sensor_msgs.msg import Joy
@@ -11,120 +12,135 @@ from gotogoal import *
 from ros_controller.srv import *
 
 
-kaas = ""
-goal = ""
+kaas = "5.0 5.0"
+goal = "5.0 7.0"
+newgoal = "6.0 7.0"
+
+oldAngleToGoal = 0.0
+
+buttonInX = False
+buttonInY = False
+buttonInStart = False
+buttonInSelect = False
 
 def callback(data):
 	for x in data.buttons:
-		if data.buttons[3] == 1:
-			print("Success LocationLogger")
-			#subprocess.call("python /home/rosw/catkin_ws/src/ros_controller/scripts/locatioinlogger.py 1", shell=True)
-			#rospy.logwarn("Success", 1)
-			print(goal)
+		if data.buttons[3] == 1 and buttonInY == False:
+			buttonInY = True
+			global buttonInY 
+
+			print("Success Y pressed")	
+			print("Goal location: " + goal)
+
 			global goal
 			write_file_goal(goal)
-		if data.buttons[7] == 1:
-			#subprocess.call("python /home/rosw/catkin_ws/src/ros_controller/src/gotogoal.py 1", shell=True)
-			#rospy.logwarn("Success", 1)
+
+		if data.buttons[3] == 0:
+			buttonInY = False
+			global buttonInY
+
+		if data.buttons[7] == 1 and buttonInStart == False:
+			buttonInStart = True
+			global buttonInStart
+
 			print("Success StartButton")
+
 			currentposX = 0.0
 			currentposY = 0.0
-			y = read_goal()
+
+			y = read_file()
+			print("Current location: " + y)
+						
 	   		y = y.split(" ")
 
         		currentPosX = float(y[0])
         		currentPosY = float(y[1])
-			print(currentPosX)
-			print(currentPosY)
-			#TODO: Get location & write to file, current is demo
-			newGoalX = 51.383839
-			newGoalY = 3.514512
-			print(newGoalX)
-			print(newGoalY)
+
+
+			x = read_goal()
+			print("Goal location: " + x)
+
+	   		x = x.split(" ")
+
+			newGoalX = float(x[0])
+			newGoalY = float(x[1])
 
 			in_x = newGoalX - currentPosX
 			in_y = newGoalY - currentPosY
-			print(in_x)
-			print(in_y)
-			#Orientation
+
 			angle_to_goal = atan2(in_y, in_x)
-			print(angle_to_goal)
-			#Change radials to degrees
+			print("Angle to goal in radians: " + str(angle_to_goal))
+
 			degree_to_goal = math.degrees(angle_to_goal)
-			print(degree_to_goal)
-			#Get old angle_to_goal
-			oldAngleToGoal = -58.3924977503
-			#if flip == True:
-			#	new_angle_to_goal - selectAngle
-			#	simAngle = selectAngle
-			#	flip = false
-			#else:
-			#	simAngle = simAngle - 0.02
-			#publisher
-			#Start turning code
-			if abs(angle_to_goal - oldAngleToGoal) > 0.1:
-				#test in real lyf
-				#speed = 0.3
-				#0.02 = 1 degree turn
-				oldAngleToGoal -= 0.3
-				speed.linear.x = 0.0
-				speed.angular.z = 0.3
+			print("Angle to goal in degrees: " + str(degree_to_goal))
+
+			if degree_to_goal < 0:
+				degree_to_goal = degree_to_goal * -1
+				
+				for i in range(10):
+					speed.linear.x = 10.0
+					//speed.angular.z = (degree_to_goal*-1.1173)
+                    speed.angular.z = (degree_to_goal*-40.1173)
+
+			elif degree_to_goal > 0:
+				
+				for i in range(10):
+					speed.linear.x = 10.0
+					//speed.angular.z = (degree_to_goal*1.1173)
+                    speed.angular.z = (degree_to_goal*40.1173)
+
+			else:
+				print("gedraaid")
+
 			pub.publish(speed)
-			#TODO: Make this repeatable theta is old 2nd
 			
-		if data.buttons[6] == 1:
+			time.sleep(2)
+			speed.linear.x = 10.0
+			speed.angular.z = 0.0	
+			pub.publish(speed)		
+
+
+		if data.buttons[7] == 0:
+			buttonInStart = False
+			global buttonInStart
+			
+		if data.buttons[6] == 1 and buttonInSelect == False:
+			buttonInSelect = True
+			global buttonInSelect 
+
 			print("Succes SelectPress")
-			print(flip)
-			homeX = 0.0
-			homeY = 0.0
-			goalX = 0.0
-			goalY = 0.0
+		
+			write_file("")
+			write_file_goal("")
 
-			x = read_file()
-	   		x = x.split(" ")
+			print("Orientation reset")
 
-        		homeX = float(x[0])
-        		homeY = float(x[1])
 
-			y = read_goal()
-	   		y = y.split(" ")
+		if data.buttons[6] == 0:
+			buttonInSelect = False
+			global buttonInSelect
 
-        		goalX = float(y[0])
-        		goalY = float(y[1])
-
-			print("HomeX: %s", homeX)
-			print("HomeY: %s", homeY)
-
-			print("GoalX: %s", goalX)
-			print("GoalY: %s", goalY)
-			inc_x = goalX - homeX
-			inc_y = goalY - homeY
-			print(inc_x)
-			print(inc_y)
-			#Orientation
-			angle_to_goal = atan2(inc_y, inc_x)
-			selectAngle = angle_to_goal
-			flip = None
-			print(angle_to_goal)
-			#Change radials to degrees
-			degree_to_goal = math.degrees(angle_to_goal)
-			print(degree_to_goal)
-		if data.buttons[2] == 1:
+		if data.buttons[2] == 1 and buttonInX == False:
 			print("Success X pressed")
-			print(kaas)
-			global kaas
-			write_file(kaas)	
-			#rospy.wait_for_service('write_service')
-			#try:
-			#	print("Success X pressed")
-			#	print(kaas)
-			#	global kaas
-			#	write_file(kaas)
-			#  	write_service = rospy.ServiceProxy('write_service', WriteFile)
-			#	resp1 = write_service(kaas)
-			#	return resp1
-			#except rospy.ServiceException, e:
-			#	print("Service call failed: %s", e)
+			buttonInX = True
+			global buttonIn
+			x = read_file()
+	   		if x == "":
+			    print("Location robot: " + kaas)
+			    global kaas
+			    write_file(kaas)
+			else:
+			    y = read_goal()
+			    write_file(y)
+			    print("Current location: " + y)
+			    print("Goal location: " + newgoal)
+		 	    global newgoal
+			    write_file_goal(newgoal)
+		if data.buttons[2] == 0:
+			buttonInX = False
+			global buttonInX
+			
+
 
 def chatterCallback(data):
 	print(data)
@@ -168,11 +184,8 @@ def start():
         # publishing to "turtle1/cmd_vel" to control turtle1
         global pub
 	global speed
-	global flip
-	global selectAngle
-	global simAngle
-        #pub = rospy.Publisher('rosaria/cmd_vel', Twist, queue_size=10)
-	pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+        pub = rospy.Publisher('rosaria/cmd_vel', Twist, queue_size=10)
+	#pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
 	speed = Twist()
         # subscribed to joystick inputs on topic "joy"
         rospy.Subscriber("joy", Joy, callback)
